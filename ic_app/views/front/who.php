e160fdcffdd70997a820b90367e3061f922757d5
Fidel <info@fidelsilva.com>
2019-01-14 09:36:38 -0500

    Front dev
    - Team photos added
    - Min height who card fixed

--- ic_app/views/front/who.php
+++ ic_app/views/front/who.php
@@ -27,7 +27,7 @@ Su misión, visión y propósito es conducir a la compañía por una ruta que le
         </p>
     </div>
     <div class="who-card col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img src="/assets/team-1.png" class="img-circle">
+        <img src="/team/Luis-Fernando-Fandino.png" class="img-circle">
         <h3>Luis Fernando <br> Fandiño Ferreira</h3>
         <p>
 Sus aportes y compromiso con Alianza han formado una relación productiva y eficiente la cual se ve representada en su involucramiento y responsabilidad en varios de los proyectos e iniciativas de la compañía, compartiendo su experiencia en el campo de la economía sumado a su conocimiento en áreas de mercadeo, financiera y administrativa, Luis Fernando Fandiño aplica la <span>estrategia y la planeación como enfoques claves que le permitirán al programa de transformación tener un desarrollo integral, eficiente y escalable.</span>
@@ -35,7 +35,7 @@ Sus aportes y compromiso con Alianza han formado una relación productiva y efic
         </p>
     </div>
     <div class="who-card col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img src="/assets/team-1.png" class="img-circle">
+        <img src="/team/Gustavo-Martinez.png" class="img-circle">
         <h3>Gustavo Adolfo <br> Martínez García</h3>
         <p>
 Conocedor del sector financiero, Gustavo Martínez cuenta con una gran experiencia en el área jurídica y de negocios. Su desempeño en la promoción y desarrollo del mercado público de valores y en la estructuración de negocios financieros y fiduciarios le ha permitido aportar en la generación de procesos y estructuras legales eficientes, dinámicas y confiables. Alianza se apoya en su <span>experiencia para sustentar desde todas las dimensiones  los propósitos y metas que tenemos trazadas como compañía.
@@ -61,7 +61,7 @@ Su trayectoria como especialista en gerencia de proyectos ligado a su conocimien
         </p>
     </div>
     <div class="who-card col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img src="/assets/team-1.png" class="img-circle">
+        <img src="/team/Andrea-Buitrago.png" class="img-circle">
         <h3>Andrea <br> Buitrago</h3>
         <p>
  Enfocada  a buscar el desarrollo del ser humano dentro de la organización, con una amplia experiencia en el rediseño de equipos, gestión por competencias, trabajo en equipo, estructuración de clima y cultura organizacional, Andrea Buitrago  aporta y genera valor para al programa de transformación aplicando estrategias que le permitan al cliente interno fortalecer y <span>desarrollar su ser y su hacer dentro de la Alianza.</span>
@@ -94,7 +94,7 @@ Su conocimiento y experiencia en las áreas contables, financieras y administrat
         </p>
     </div>
     <div class="who-card col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img src="/assets/team-1.png" class="img-circle">
+        <img src="/team/Ivan-Ordonez.png" class="img-circle">
         <h3>Iván Darío <br>Ordoñez Arango</h3>
         <p>
 Su amplio conocimiento en el sector económico, especialmente en el sector financiero, aplicando bases de ingeniería, sistemas, comerciales y de control para el diseño de estrategias que optimicen procesos, generen valor y competitividad frente al sector, son las características que definen a Iván Darío Ordoñez quien considera que el proyecto de transformación es una herramienta e insumo capaz <span>de dinamizar integralmente a la compañía si se aprovechan y optimizan los procesos y los recursos con los que cuenta Alianza.</span>
@@ -102,7 +102,7 @@ Su amplio conocimiento en el sector económico, especialmente en el sector finan
         </p>
     </div>
     <div class="who-card col-lg-4 col-md-4 col-sm-4 col-xs-12">
-        <img src="/assets/team-1.png" class="img-circle">
+        <img src="/team/Johann-Quiroga.png" class="img-circle">
         <h3>Johann Alejandro <br>Quiroga</h3>
         <p>
 Su experiencia y conocimiento en la implementación y aplicación de modelos de madurez en desarrollo de Software, gerenciando y gestionando proyectos y operaciones tecnológicas que impulsen a la organización y le permitan trascender y posicionarse en el mercado actual como futuro, es la propuesta de valor que Johann Alejandro Quiroga le aporta a Alianza y al proyecto <span>de transformación, buscando que la sistematización y la consolidación de automatismos a partir del aprovechamiento del recurso digital, logren potencializar la empresa y ser adoptadas e integradas en la cultura corporativa de la 
